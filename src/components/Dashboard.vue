33dc0da66306134a6a84a994e0eed58b62e17861
lsbyerley <lsbyerley@gmail.com>
2018-04-17 15:15:25 -0400

    updates to store and dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,50 +1,91 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-  </div>
+
+  <v-layout align-center justify-center>
+    <v-flex xs12 sm8 md4>
+      <v-card class="elevation-12">
+        <v-toolbar dark color="primary">
+          <v-toolbar-title>{{ label }}</v-toolbar-title>
+          <v-spacer></v-spacer>
+        </v-toolbar>
+        <v-card-text>
+          <v-form>
+            <v-switch
+              disabled
+              :label="`Connected: ${connected}`"
+              v-model="connected"
+            ></v-switch>
+            <v-switch
+              true-value="on"
+              false-value="off"
+              :label="`Power: ${power}`"
+              v-model="power"
+            ></v-switch>
+            <v-slider :min="minKelvin" :max="maxKelvin" label="Kelvin" v-model="kelvin"></v-slider>
+            <v-slider :min="0" :max="1" label="Brightness" v-model="brightness"></v-slider>
+            <v-slider :min="0" :max="100" label="Hue" v-model="hue"></v-slider>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn v-on:click="updateBulbState" color="primary">Update Bulb</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex'
+import { mapFields } from 'vuex-map-fields'
 
 export default {
   name: 'Dashboard',
-  mounted() {
-    console.log(this.$store)
-    this.$store.dispatch('increment')
+  created() {
     if (!this.$store.state.bulbStatusLoaded) {
       this.$store.dispatch('getBulbStatus')
     }
   },
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  },
   computed: {
-    ...mapState([
-      'bulbStatusLoaded',
-      'bulbStatus'
+    ...mapFields([
+      'bulbStatus.connected',
+      'bulbStatus.label',
+      'bulbStatus.power',
+      'bulbStatus.kelvin',
+      'bulbStatus.minKelvin',
+      'bulbStatus.maxKelvin',
+      'bulbStatus.brightness',
+      'bulbStatus.hue'
     ])
+  },
+  methods: {
+    updateBulbState() {
+      console.log('UPDATE')
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style lang="scss" scoped>
+.columns {
+  display: flex;
+  justify-content: center;
+  vertical-align: center;
+  align-items: center;
+  height: 100vh;
+  background: #F7F7F7;
+
+  .card {
+    .card-header {
+      padding: 1rem;
+      background: #00b89c;
+
+      .card-header-title {
+        font-size: 1.5rem;
+        padding: 0;
+      }
+    }
+  }
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import LIFX from 'lifx-http-api'
+import { getField, updateField } from 'vuex-map-fields';
 
 let lifxClient = new LIFX({
   bearerToken: 'my-token'
@@ -11,33 +12,62 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   strict: true,
   state: {
-    bulbStatus: {},
+    bulbStatus: {
+      id: '',
+      label: '',
+      connected: '',
+      power: '',
+      brightness: '',
+      hue: '',
+      saturation: '',
+      kelvin: '',
+      minKelvin: '',
+      maxKelvin: ''
+    },
     bulbStatusLoaded: false
   },
   actions: {
     async getBulbStatus({ commit }) {
       try {
-        //const res = await this.$axios.get('/api/bulb-status')
-        const clientRes = await lifxClient.listLights('all');
-        commit('setBulbStatus', { bulbStatus: clientRes.data })
+        const clientRes = await lifxClient.listLights(bulbId);
+        commit('setBulbStatus', { bulbStatus: clientRes })
         commit('setBulbStatusLoaded', { bulbStatusLoaded: true })
       } catch(err) {
         console.error(err)
       }
     },
-    increment ({ commit }) {
-      console.log('increment')
+    async updateBulbState({commit}, payload) {
+      try {
+        const clientRes = await lifxClient.setState('all', {
+          power: payload.power,
+          //color: 'blue saturation:0.5',
+          color: payload.color,
+          brightness: payload.brightness
+        })
+      } catch(err) {
+
+      }
     }
   },
+  getters: {
+    getField
+  },
   mutations: {
+    updateField,
     setBulbStatus: (state, { bulbStatus }) => {
-      state.bulbStatus = bulbStatus[0]
+      state.bulbStatus.id = bulbStatus[0].id
+      state.bulbStatus.label = bulbStatus[0].label
+      state.bulbStatus.connected = bulbStatus[0].connected
+      state.bulbStatus.power = bulbStatus[0].power
+      state.bulbStatus.brightness = bulbStatus[0].brightness
+      state.bulbStatus.hue = bulbStatus[0].color.hue
+      state.bulbStatus.saturation = bulbStatus[0].color.saturation
+      state.bulbStatus.kelvin = bulbStatus[0].color.kelvin
+      state.bulbStatus.minKelvin = bulbStatus[0].product.capabilities.min_kelvin
+      state.bulbStatus.maxKelvin = bulbStatus[0].product.capabilities.max_kelvin
     },
     setBulbStatusLoaded: (state, { bulbStatusLoaded }) => {
       state.bulbStatusLoaded = bulbStatusLoaded
-    },
-  },
-  actions: {
-
+    }
   }
 })
